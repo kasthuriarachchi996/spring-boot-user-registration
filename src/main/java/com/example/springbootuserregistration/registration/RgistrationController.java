3a596c1b9fe988f027d014ae4f88a56098e7b0bd
kasthuriarachchi996 <www.pasindudenuka5770@gmail.com>
2023-04-05 18:06:51 +0530

    build user details

--- src/main/java/com/example/springbootuserregistration/registration/RgistrationController.java
+++ src/main/java/com/example/springbootuserregistration/registration/RgistrationController.java
@@ -1,2 +1,19 @@
-package com.example.springbootuserregistration.registration;public class RgistrationController {
+package com.example.springbootuserregistration.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/vi")
+@AllArgsConstructor
+public class RgistrationController {
+
+    private final RegisterService registerService;
+
+    public String register(@RequestBody RegistrationRequest request)
+    {
+        return registerService.register(request);
+    }
 }
